6354f2a7355b3b08111f22b26d87af78f2847c1b
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-06-14 18:50:27 -0400

    create ProductList

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import sampleData from '@/db/sample-data';
+import ProductList from '@/components/shared/product/product-list';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </div>
+  );
 };
 
 export default HomePage;
