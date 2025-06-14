6472b45237a52f4ea1faec690019ed9293688562
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-06-14 15:12:55 -0400

    header update

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+import Header from '@/components/shared/header';
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,6 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <div className="flex flex-col h-screen">
+      <Header />
       <main className="flex-1 wrapper">{children}</main>
     </div>
   );
