3cd9ed03a4d8484046ff0f0692f250854298f5f1
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-06-14 18:54:27 -0400

    create ProductCard

---
+++ components/shared/product/product-card.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Card, CardHeader } from '@/components/ui/card';
+
+const ProductCard = ({ product }: { product: any }) => {
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader className="p-0 items-center">
+        <Link href={`/product/${product.slug}`}>
+          <Image
+            src={product.images[0]}
+            alt={product.name}
+            height={300}
+            width={300}
+            priority={true}
+          />
+        </Link>
+      </CardHeader>
+    </Card>
+  );
+};
+
+export default ProductCard;
