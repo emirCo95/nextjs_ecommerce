8b149e13b3c9c3f8958912443f077c37c17a0997
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-06-14 18:58:08 -0400

    Update product-card.tsx

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Card, CardHeader } from '@/components/ui/card';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
 
 const ProductCard = ({ product }: { product: any }) => {
   return (
@@ -17,6 +17,20 @@ const ProductCard = ({ product }: { product: any }) => {
           />
         </Link>
       </CardHeader>
+      <CardContent className="p-4 grid gap-4">
+        <div className="text-xs">{product.brand}</div>
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="text-sm font-medium">{product.name}</h2>
+        </Link>
+        <div className="flex-between gap-4">
+          <p>{product.rating} stars</p>
+          {product.stock > 0 ? (
+            <p className="font-bold">{product.price}</p>
+          ) : (
+            <p className="text-destructive">Out Of Stock</p>
+          )}
+        </div>
+      </CardContent>
     </Card>
   );
 };
